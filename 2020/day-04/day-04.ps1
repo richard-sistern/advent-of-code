cd5dd3707daf9d8e47cc42694d7f640e56f387d9
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-12-07 17:53:03 +0000

    Day 04 start

---
+++ 2020/day-04/day-04.ps1
@@ -0,0 +1,17 @@
+
+
+function Invoke-Part1 {
+  param (
+
+  )
+
+  Return $FALSE
+} # Invoke-Part1
+
+function Invoke-Part2 {
+  param (
+
+  )
+
+  Return $FALSE
+} # Invoke-Part2
---
+++ 2020/day-04/day-04.tests.ps1
@@ -0,0 +1,13 @@
+BeforeAll {
+  . $PSCommandPath.Replace('.tests.ps1', '.ps1')
+
+
+Describe "Part01" {
+  It "Returns correct number of trees" {
+
+  }
+}
+
+Describe "Part02" {
+  
+}
