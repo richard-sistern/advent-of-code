7a199105ac05b051dd1fdbbdf06f7f63c4ab8de9
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-12-07 17:43:48 +0000

    Day 03 finished

--- 2020/day-03/day-03.ps1
+++ 2020/day-03/day-03.ps1
@@ -1,26 +1,58 @@
+function Check-Slope {
+  param (
+    [string[]] $map,
+    [int] $right,
+    [int] $down
+  )
+
+  [int] $line_count = 0
+  [int] $trees = 0
+  [int] $x = 0
+
+  Foreach ($line in $map)
+  {
+    if ([array]::IndexOf($map, $line) -eq $line_count)
+    {
+      if ($x -gt $line.length -1) {
+        $x = $x - $line.Length
+      }
+
+      if ($line[$x] -eq '#') {
+        $trees += 1
+      }
+
+      $line_count += $down
+
+      $x += $right
+    }
+  }
+
+  $trees
+  } # Check-Slope
+
 function Invoke-Part1 {
   param (
     [string[]] $map
   )
 
-  [int] $x = 0
-  [int] $trees = 0
+  Check-Slope -Map $map -Right 3 -Down 1
+} # Invoke-Part1
 
-  Foreach ($line in $map)
-  {
-    # $x = 0, $line.length = 1
-    if ($x -gt $line.length - 1) {
-      $x = $x - $line.Length
-    }
+function Invoke-Part2 {
+  param (
+    [string[]] $map
+  )
 
-    if ($line[$x] -eq '#') {
-      $trees += 1
-    }
+  $total = 1
 
-    $x += 3
-  }
-  $trees
-}
+  $total = $total * (Check-Slope -Map $map -right 1 -down 1)
+  $total = $total * (Check-Slope -Map $map -right 3 -down 1)
+  $total = $total * (Check-Slope -Map $map -right 5 -down 1)
+  $total = $total * (Check-Slope -Map $map -right 7 -down 1)
+  $total = $total * (Check-Slope -Map $map -right 1 -down 2)
+
+  $total
+} # Invoke-Part2
 
 #[string[]] $map = Get-Content $PSCommandPath.Replace('.ps1', '-input.txt')
 #Invoke-Part1 -Map $map
--- 2020/day-03/day-03.tests.ps1
+++ 2020/day-03/day-03.tests.ps1
@@ -21,3 +21,27 @@ Describe "Part01" {
     Invoke-Part1 -Map $map | Should -Be 7
   }
 }
+
+Describe "Part02" {
+  It "Returns connect number of trees" {
+    Check-Slope -Map $map -Right 1 -Down 1 | Should -Be 2
+  }
+  It "Returns connect number of trees" {
+    Check-Slope -Map $map -Right 3 -Down 1 | Should -Be 7
+  }
+  It "Returns connect number of trees" {
+    Check-Slope -Map $map -Right 5 -Down 1 | Should -Be 3
+  }
+  It "Returns connect number of trees" {
+    Check-Slope -Map $map -Right 7 -Down 1 | Should -Be 4
+  }
+  It "Returns connect number of trees" {
+    Check-Slope -Map $map -Right 1 -Down 2 | Should -Be 2
+  }
+
+  It "Returns correct number of trees for all slopes" {
+    $slopes = @(@{right = 1; down = 1}, @{right = 3; down = 1}, @{right = 5; down = 1}, @{right = 7; down = 1}, @{right = 1; down = 2})
+    Invoke-Part2 -Map $map -Slopes $slopes | Should -Be 336
+  }
+
+}
