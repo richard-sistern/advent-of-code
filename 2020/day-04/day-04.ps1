d854ab744d1fec96a741e49737d2de3dfe6b7c48
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2020-12-09 13:27:56 +0000

    Day 04 Part 2 finished

--- 2020/day-04/day-04.ps1
+++ 2020/day-04/day-04.ps1
@@ -1,4 +1,73 @@
 function Invoke-Part1 {
+param (
+  $passports
+)
+
+$passport = @{}
+$count = 0
+
+  ForEach ($line in $passports) {
+    if ($line) {
+      $line.Split(' ') | ForEach-Object {
+        $key, $value = $_.Split(':')
+        $passport.Add($key, $value)
+      }
+    }
+    else {
+      # Blank line
+      If ($passport.byr -and $passport.iyr -and $passport.eyr -and $passport.hgt -and $passport.hcl -and $passport.ecl -and $passport.pid) {
+        $count += 1
+      }
+      $passport = @{}
+    }
+  }
+
+  $count
+} # Invoke-Part1
+
+  # $passports = Get-Content .\day-04-input.txt
+  # Invoke-Part1 -passports $passports
+
+function Test-Passport {
+param (
+  [ValidateRange(1920, 2002)]
+  $byr,
+  [ValidateRange(2010, 2020)]
+  $iyr,
+  [ValidateRange(2020, 2030)]
+  $eyr,
+  [ValidatePattern("^\d+(cm|in)")]
+  [ValidateScript({
+    $num = $_ -replace "\D"
+    if ($_ -match "cm") {
+      if ($num -ge 150 -and $num -le 193) { $true }
+    }
+    else {
+      if ($num -ge 59 -and $num -le 76) { $true }
+    }
+    })]
+    $hgt,
+    [ValidatePattern("^#[\da-f]{6}$")]
+    $hcl,
+    [ValidateSet("amb", "blu", "brn", "gry", "grn", "hzl", "oth")]
+    $ecl,
+    [ValidatePattern("^\d{9}$")]
+    $pid_
+  )
+  if (
+    $byr -and
+    $iyr -and
+    $eyr -and
+    $hgt -and
+    $hcl -and
+    $ecl -and
+    $pid_
+  ) {
+    $true
+  }
+  } # Test-Passport
+
+function Invoke-Part2 {
   param (
     $passports
   )
@@ -15,14 +84,18 @@ function Invoke-Part1 {
     }
     else {
       # Blank line
-      If ($passport.byr -and $passport.iyr -and $passport.eyr -and $passport.hgt -and $passport.hcl -and $passport.ecl -and $passport.pid) { 
-        $count += 1
+      try {
+        if (Test-Passport -byr $passport.byr -iyr $passport.iyr -eyr $passport.eyr -hgt $passport.hgt -hcl $passport.hcl -ecl $passport.ecl -pid_ $passport.pid -ea stop) {
+          $count += 1
+        }
       }
+      catch {  }
+
       $passport = @{}
     }
   }
   $count
-} # Invoke-Part1
+} # Invoke-Part2
 
-# $passports = Get-Content .\day-04-input.txt
-# Invoke-Part1 -passports $passports
+    #$passports = Get-Content $PSCommandPath.Replace('.ps1', '-input.txt')
+    #Invoke-Part2 -passports $passports
--- 2020/day-04/day-04.tests.ps1
+++ 2020/day-04/day-04.tests.ps1
@@ -1,15 +1,19 @@
 BeforeAll {
   . $PSCommandPath.Replace('.tests.ps1', '.ps1')
 
-  
-  #$passports = Get-Content .\day-04-test-input.txt
+    $passports = Get-Content $PSCommandPath.Replace('.tests.ps1', '-test-input.txt')
 }
 
-$passports 
 Describe "Part01" {
   It "Returns correct number of valid passports" {
-    $passports = Get-Content $PSCommandPath.Replace('.tests.ps1', '-test-input.txt')
-    Invoke-Part1 -Passports $passports | Should -Be 2
+    
+    Invoke-Part1 -Passports $passports | Should -Be 7
+  }
+}
+
+Describe "Part02" {
+  It "Returns correct number of valid passports" {
+    Invoke-Part2 -Passports $passports | Should -Be 3
   }
 }
 
