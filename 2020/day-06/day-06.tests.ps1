06fe21e3f46f5dcafa86e29ebf816b9ef643fe2a
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-12-10 16:36:50 +0000

    Revert "Day 06 finished"
    This reverts commit bd8000816cce995da3b01854794de4e1df3d2caf.

--- 2020/day-06/day-06.tests.ps1
+++ 2020/day-06/day-06.tests.ps1
@@ -1,10 +1,9 @@
 BeforeAll {
     . $PSCommandPath.Replace('.tests.ps1', '.ps1')
-    $forms = Get-Content $PSCommandPath.Replace('.tests.ps1', '-test-input.txt') -Delimiter ([System.Environment]::NewLine * 2)
 }
 
-Describe "day-06 part-01" {
+Describe "day-06" {
     It "Returns expected output" {
-        Invoke-Part1 -forms $forms | Should -Be 11
+        day-06 | Should -Be "YOUR_EXPECTED_VALUE"
     }
 }
