d08650d130c219773034d571bd45f4fff60c9e60
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-12-10 16:38:46 +0000

    Revert "Revert "Day 06 finished""
    This reverts commit 06fe21e3f46f5dcafa86e29ebf816b9ef643fe2a.

--- 2020/day-06/day-06.ps1
+++ 2020/day-06/day-06.ps1
@@ -1,3 +1,13 @@
-function day-06 {
-    throw [NotImplementedException]'day-06 is not implemented.'
-}
+function Invoke-Part1 {
+    param (
+        $forms
+    )
+    #$Questions = Get-Content .\6.input.txt -Delimiter "`n`n"
+    $total = $forms | ForEach-Object {
+        ( $_ -split "" | Where-Object { $_ -match "[a-z]" } | Select-Object -Unique  ).Count
+    } | Measure-Object -Sum
+    $total.Sum
+} # Invoke-Part1
+
+#$forms = Get-Content $PSCommandPath.Replace('.ps1', '-input.txt') -Delimiter ([System.Environment]::NewLine * 2)
+#Invoke-Part1 -forms $forms
--- 2020/day-06/day-06.tests.ps1
+++ 2020/day-06/day-06.tests.ps1
@@ -1,9 +1,10 @@
 BeforeAll {
     . $PSCommandPath.Replace('.tests.ps1', '.ps1')
+    $forms = Get-Content $PSCommandPath.Replace('.tests.ps1', '-test-input.txt') -Delimiter ([System.Environment]::NewLine * 2)
 }
 
-Describe "day-06" {
+Describe "day-06 part-01" {
     It "Returns expected output" {
-        day-06 | Should -Be "YOUR_EXPECTED_VALUE"
+        Invoke-Part1 -forms $forms | Should -Be 11
     }
 }
