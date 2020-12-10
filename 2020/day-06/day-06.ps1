b611a2ad8011f6639144d6b8a74fd9e9012a4790
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-12-10 19:27:00 +0000

    Update day-06.ps1

--- 2020/day-06/day-06.ps1
+++ 2020/day-06/day-06.ps1
@@ -2,7 +2,7 @@ function Invoke-Part1 {
     param (
         $forms
     )
-    #$Questions = Get-Content .\6.input.txt -Delimiter "`n`n"
+    
     $total = $forms | ForEach-Object {
         ( $_ -split "" | Where-Object { $_ -match "[a-z]" } | Select-Object -Unique ).Count
     } | Measure-Object -Sum
