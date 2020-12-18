247c1ff95bc7a2f15a4f128ce2a25432d20c61af
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-12-18 18:15:21 +0000

    Day 8 Part 1 finished

--- 2020/day-08/day-08.ps1
+++ 2020/day-08/day-08.ps1
@@ -1,3 +1,33 @@
-function day-08 {
-    throw [NotImplementedException]'day-08 is not implemented.'
-}
+function Invoke-Part1 {
+    param (
+        $instructions
+    )
+
+    #$queue = New-Object System.Collections.Queue # Might be handy in part 2!
+    $i = 0
+    $accumulator = 0
+    $queue_flag = "ON QUEUE"
+
+    while ($i -lt $instructions.length) {
+        if ($instructions[$i] -eq $queue_flag) { break }
+
+        #$queue.Enqueue($instructions[$i])
+        $instruction = $instructions[$i] -split " "
+
+        $instructions[$i] = $queue_flag
+            
+        $op = $instruction[0]
+        [double] $arg = $instruction[1]
+            
+        switch ($op) {
+            'acc' { $accumulator += $arg; $i++ }
+            'jmp' { $i += $arg  }
+            'nop' { $i++ }
+        }
+    }
+    $accumulator
+} # Invoke-Part1
+
+#$instructions = Get-Content $PSCommandPath.Replace('.ps1', '-input.txt') # | Select-Object -first 20
+#Invoke-Part1 -instructions $instructions
+
--- 2020/day-08/day-08.tests.ps1
+++ 2020/day-08/day-08.tests.ps1
@@ -1,9 +1,21 @@
 BeforeAll {
     . $PSCommandPath.Replace('.tests.ps1', '.ps1')
+
+    $instructions = @(
+        "nop +0",
+        "acc +1",
+        "jmp +4",
+        "acc +3",
+        "jmp -3",
+        "acc -99",
+        "acc +1",
+        "jmp -4",
+        "acc +6"
+    )
 }
 
-Describe "day-08" {
-    It "Returns expected output" {
-        day-08 | Should -Be "YOUR_EXPECTED_VALUE"
+Describe "day-08 part-01" {
+    It "Returns expected accumulator" {
+        Invoke-Part1 -instructions $instructions | Should -Be 5
     }
 }
