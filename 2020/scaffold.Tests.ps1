3b9fd7076a812cbba91a17dd7e0c7df707cd3cc3
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-12-19 07:59:53 +0000

    Updated scaffold script

--- 2020/scaffold.Tests.ps1
+++
@@ -1,9 +0,0 @@
-BeforeAll {
-    . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
-}
-
-Describe "aoc_scaffold" {
-    It "Returns expected output" {
-        aoc_scaffold | Should -Be "YOUR_EXPECTED_VALUE"
-    }
-}
--- 2020/scaffold.ps1
+++ 2020/scaffold.ps1
@@ -1,4 +1,4 @@
-function aoc_scaffold {
+function Add-Scaffold {
     param(
         $day
     )
@@ -6,21 +6,16 @@ function aoc_scaffold {
     $day = "day-{0:D2}" -f $day
     $path = "$($PSCommandPath.Replace('scaffold.ps1', ''))$day"
     
-   # $day
-
-    $path
     If (Test-Path $path -PathType Container) {
         "$day already exists, wonder how you answered it..."
     }
     else {
-        
         New-Item -ItemType Directory $path
         New-Item -ItemType File "$path\README.txt"
         New-Item -ItemType File "$path\$day-input.txt"
         New-Item -ItemType File "$path\$day.ps1"
         New-Item -ItemType File "$path\$day.tests.ps1"
     }
-    #throw [NotImplementedException]'aoc_scaffold is not implemented.'
 }
 
-01..24 | ForEach-Object { aoc_scaffold $_ }
+01..24 | ForEach-Object { Add-Scaffold $_ }
