279316221ae9bc3e61dae255c9e779520b769bb5
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-12-05 20:53:15 +0000

    Start day 2

---
+++ 2022/day-02/day-02.py
@@ -0,0 +1,13 @@
+# Possible Game Outcomes
+
+# | P1 | P2 | Result | Score       |
+# |----|----|--------|-------------|
+# | A  | X  | Draw   | (1 + 3) = 4 |
+# | A  | Y  | Win    | (2 + 6) = 8 |
+# | A  | Z  | Loss   | (3 + 0) = 3 |
+# | B  | X  | Loss   | (1 + 0) = 1 |
+# | B  | Y  | Draw   | (2 + 3) = 5 |
+# | B  | Z  | Win    | (3 + 6) = 9 |
+# | C  | X  | Win    | (1 + 6) = 7 |
+# | C  | Y  | Loss   | (2 + 0) = 2 |
+# | C  | Z  | Draw   | (3 + 3) = 6 |
