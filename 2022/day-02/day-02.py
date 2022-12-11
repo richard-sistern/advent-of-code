9d30e070be7d466236468e6174b4e3a22fbda219
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-12-11 14:44:03 +0000

    Finish day 2

--- 2022/day-02/day-02.py
+++ 2022/day-02/day-02.py
@@ -1,5 +1,4 @@
 # Possible Game Outcomes
-
 # | P1 | P2 | Result | Score       |
 # |----|----|--------|-------------|
 # | A  | X  | Draw   | (1 + 3) = 4 |
@@ -12,24 +11,54 @@
 # | C  | Y  | Loss   | (2 + 0) = 2 |
 # | C  | Z  | Draw   | (3 + 3) = 6 |
 
+def part_1(games):
+    outcomes = {
+        "A X":4,
+        "A Y":8,
+        "A Z":3,
+        "B X":1,
+        "B Y":5,
+        "B Z":9,
+        "C X":7,
+        "C Y":2,
+        "C Z":6
+    }
+
+    score = 0
+
+    for game in games:
+        score += outcomes[game]
+
+    return(score)
+
+# X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+def part_2(games):
+    outcomes = {
+        "A X":3,
+        "A Y":4,
+        "A Z":8,
+        "B X":1,
+        "B Y":5,
+        "B Z":9, 
+        "C X":2,
+        "C Y":6,
+        "C Z":7 
+    }
+
+    score = 0
+
+    for game in games:
+        score += outcomes[game]
+
+    return(score)
+
+
 with open("input.txt") as f:
-    games = f.read().splitlines() 
+    games = f.read().splitlines()
 
-outcomes = {
-    "A X":4,
-    "A Y":8,
-    "A Z":3,
-    "B X":1,
-    "B Y":5,
-    "B Z":9,
-    "C X":7,
-    "C Y":2,
-    "C Z":6
-}
+    result = part_1(games)
+    print(f'Part 1 result: {result}')
 
-score = 0
+    result = part_2(games)
+    print(f'Part 2 result: {result}')
 
-for game in games:
-    score += outcomes[game]
-
-print(score)
