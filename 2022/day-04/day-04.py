76df2d9d5290dbe5a86419b010d0969705eabfcc
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2023-03-28 20:00:27 +0100

    Finish day 4

--- 2022/day-04/day-04.py
+++ 2022/day-04/day-04.py
@@ -12,10 +12,30 @@ def part1(assignments):
 
     return overlaps
 
+def part2(assignments):
+    overlaps = 0
 
+    for assignment in assignments:
+        left, right = assignment.split(",")
+
+        left_start, left_end = [int(i) for i in left.split("-")]
+        right_start, right_end = [int(i) for i in right.split("-")]
+
+        if right_end >= left_start >= right_start or left_end >= right_start >= left_start:
+            overlaps += 1
+
+    return overlaps
+
+    aa, aaa = [int(r) for r in a.split("-")]
+    bb, bbb = [int(r) for r in b.split("-")]
+
+    if bbb >= aa >= bb or aaa >= bb >= aa:t+=1
 
 with open("input.txt") as f:
     assignments = f.read().splitlines()
 
-result = part1(assignments)
-print(result)
+result1 = part1(assignments)
+print(result1)
+
+result2 = part2(assignments)
+print(result2)
