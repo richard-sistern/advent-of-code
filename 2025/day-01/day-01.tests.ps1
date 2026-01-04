b931b03515f79549257fc8fed6b0b7a368fa7ddf
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2026-01-04 20:14:35 +0000

    Fix test

--- 2025/day-01/day-01.tests.ps1
+++ 2025/day-01/day-01.tests.ps1
@@ -10,6 +10,6 @@ Describe 'Part 01' {
 
 Describe 'Part 02' {
   It 'Calculates the correct password' {
-    Invoke-Part1 -Rotations @("L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82")  | Should -Be 6
+    Invoke-Part2 -Rotations @("L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82")  | Should -Be 6
   }
 }
