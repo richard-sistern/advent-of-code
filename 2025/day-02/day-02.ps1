fa3101ad162606a29e04dc4f8c687b7cbbc5e2bc
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2026-01-12 20:28:15 +0000

    Finish day 2 part 2

--- 2025/day-02/day-02.ps1
+++ 2025/day-02/day-02.ps1
@@ -25,6 +25,33 @@ function Invoke-Part1 {
   return $sum
 } # Invoke-Part1
 
-$ranges = Get-Content $PSCommandPath.Replace('.ps1', '-input.txt')
+function Invoke-Part2 {
+  [CmdletBinding()]
 
+  param (
+    [string[]] $ranges
+  )
+
+  $sum = 0
+
+  Foreach ($range in $ranges)
+  {
+    $min, $max = $range -split '-' -as [int64[]]
+    Write-Verbose "Processing range: $range from $min to $max"
+    for ($number = $min; $number -le $max; $number++) {
+      # ^     — start of the string
+      # (\d+) — capture group 1: one or more digits
+      # \1+   — one or more repetitions of whatever was captured in group 1
+      # $     — end of the string
+      if ($number -match '^(\d+)\1+$') {
+        $sum += $number
+      }
+    }
+  }
+
+  return $sum
+} # Invoke-Part2
+
+$ranges = Get-Content $PSCommandPath.Replace('.ps1', '-input.txt')
 Invoke-Part1 -ranges $ranges.Split(',') -Verbose
+Invoke-Part2 -ranges $ranges.Split(',') -Verbose
--- 2025/day-02/day-02.tests.ps1
+++ 2025/day-02/day-02.tests.ps1
@@ -1,13 +1,22 @@
 BeforeAll {
  . $PSCommandPath.Replace('.tests.ps1', '.ps1')
+
+ $ranges = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124".Split(',')
 }
 
 Describe 'Part 01' {
   It 'Calculate the sum of invalid IDs' {
-    $ranges = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-824824821-824824827,2121212118-2121212124".Split(',')
+    
     Invoke-Part1 -Ranges $ranges | Should -Be 1227775554
+  }
+}
+
+Describe 'Part 02' {
+  It 'Calculate the sum of invalid IDs' {
+    
+    Invoke-Part2 -Ranges $ranges | Should -Be 4174379265
   }
 }
 
